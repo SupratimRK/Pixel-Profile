b83cf9696347067a00f69322f5962bf6789f244f
Supratim <supratimrk@gmail.com>
2023-04-22 16:36:40 +0530

    Minor UI tweak

--- script.js
+++ script.js
@@ -3,3 +3,7 @@ document.addEventListener('mousemove',(e) => {
     cursor.style.left = e.pageX + 'px';
     cursor.style.top = e.pageY + 'px';
 })
+
+if (screen.width <= 1199) { document.location = "alert.html"; }
+
+//document.getElementById('foot').style.visibility = "hidden";
