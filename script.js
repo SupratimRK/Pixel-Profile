900c4ec6ffc17cc580b2bb14b3e0558f8f0f48af
Supratim <supratimrk@gmail.com>
2023-04-24 20:44:08 +0530

    Added theme

--- script.js
+++ script.js
@@ -7,3 +7,8 @@ document.addEventListener('mousemove',(e) => {
 if (screen.width <= 1199) { document.location = "alert.html"; }
 
 //document.getElementById('foot').style.visibility = "hidden";
+
+var icon = document.getElementById("icon");
+icon.onclick = function (){
+    document.body.classList.toggle("dark");
+}
